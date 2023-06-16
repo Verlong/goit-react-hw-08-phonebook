ecc5b2edbffe0e1a30b6ca6cc3f5fce9ade171b3
Maryna Bonko <bonko23@gmail.com>
2023-06-16 13:53:29 +0300

    fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import ContactList from 'components/contact-list/contact-list';
 import css from './pages.module.css';
+import { useAuth } from 'hooks';
+
 export default function Home() {
+  const { isLoggedIn } = useAuth();
+
   return (
     <div>
       <h1 className={css.title}>Your Phonebook</h1>
-      {ContactList ? <ContactList /> : <p>Please Log in</p>}
+      {isLoggedIn ? <ContactList /> : <p>Please Log in</p>}
     </div>
   );
 }
