ef4f7af6ccd4e22b9e6f65fa7417d68e0fe0de46
Maryna Bonko <bonko23@gmail.com>
2023-06-16 13:41:01 +0300

    centered title and add contacts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
+import ContactList from 'components/contact-list/contact-list';
+import css from './pages.module.css';
 export default function Home() {
   return (
     <div>
-      <h1>Your Phonebook</h1>
+      <h1 className={css.title}>Your Phonebook</h1>
+      {ContactList ? <ContactList /> : <p>Please Log in</p>}
     </div>
   );
 }
